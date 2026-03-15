2db5348df2d1035904146f1ed190d16a156c64f7
fewtarius <fewtarius@steamfork.org>
2026-03-14 22:23:03 -0400

    chore: add PhotonTERM cask 20260314.1

---
+++ Casks/photonterm.rb
@@ -0,0 +1,22 @@
+cask "photonterm" do
+  version "20260314.1"
+  sha256 :no_check
+
+  url "https://github.com/fewtarius/PhotonTERM/releases/download/#{version}/PhotonTERM-#{version}.dmg"
+  name "PhotonTERM"
+  desc "BBS terminal client - ANSI/VT100, SSH, Telnet, file transfer"
+  homepage "https://github.com/fewtarius/PhotonTERM"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "PhotonTERM.app"
+
+  zap trash: [
+    "~/Library/Application Support/PhotonTERM",
+    "~/Library/Preferences/org.fewtarius.PhotonTERM.plist",
+    "~/Library/Caches/org.fewtarius.PhotonTERM",
+  ]
+end
